c33ab71d02deefd8f65f362bc7fac733fda72867
Alejandro Vega <alejovegam@gmail.com>
2020-08-31 10:44:51 -0500

    validation - input must have characters

---
+++ 01.js
@@ -0,0 +1,47 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    titulo: "listado de tareas",
+    tareas: [],
+    nuevaTarea: "",
+    validar:false
+  },
+
+  methods: {
+    agregarTarea() {
+      //console.log('diste click ', this.nuevaTarea);
+      if (this.nuevaTarea == false) {
+        alert("Ingresa un nombre a tu tarea");
+        return;
+      }else {
+        this.tareas.push({
+          nombre: this.nuevaTarea,
+          estado: false,
+        });
+      }
+      this.nuevaTarea = "";
+      localStorage.setItem("gym-vue", JSON.stringify(this.tareas));
+    },
+    cambioEstado(index) {
+      //console.log("Diste click", index);
+      this.tareas[index].estado = true;
+      localStorage.setItem("gym-vue", JSON.stringify(this.tareas));
+    },
+    borrar(index) {
+      //console.log("clickborrar", index);
+      this.tareas.splice(index, 1);
+      localStorage.setItem("gym-vue", JSON.stringify(this.tareas));
+    },
+  },
+
+  created: function () {
+    let datosDB = JSON.parse(localStorage.getItem("gym-vue"));
+    if (datosDB == null) {
+      this.tareas = [];
+    } else {
+      this.tareas = datosDB;
+    }
+  },
+
+  
+});
