2d3a3d6420251b40930b3a2e2ff53ff6c842e320
Alejandro Vega <alejovegam@gmail.com>
2020-09-01 16:04:05 -0500

    inclusión de contador de tareas

--- 01.js
+++ 01.js
@@ -4,49 +4,66 @@ const app = new Vue({
     titulo: "listado de tareas",
     tareas: [],
     nuevaTarea: "",
-    modificacion:""
+    modificacion:"",
+    contador:0
   },
 
   methods: {
     agregarTarea() {
       //console.log('diste click ', this.nuevaTarea);
-      if (this.nuevaTarea == false) {
+      if (!this.nuevaTarea) {
         alert("Ingresa una tarea");
       } else {
+        this.contador++
         this.tareas.push({
           nombre: this.nuevaTarea,
           estado: false,
         });
       }
       this.nuevaTarea = "";
-      localStorage.setItem("gym-vue", JSON.stringify(this.tareas));
+      localStorage.setItem("my-vue", JSON.stringify(this.tareas));
     },
     cambioEstado(index) {
       //console.log("Diste click", index);
+      if(this.tareas[index].estado == false){
       this.tareas[index].estado = true;
-      localStorage.setItem("gym-vue", JSON.stringify(this.tareas));
+      this.contador--}
+       
+      localStorage.setItem("my-vue", JSON.stringify(this.tareas));
     },
     borrar(index) {
       //console.log("clickborrar", index);
+      if (this.tareas[index].estado == false) {
+      this.contador--
+      }
       this.tareas.splice(index, 1);
-      localStorage.setItem("gym-vue", JSON.stringify(this.tareas));
+      localStorage.setItem("my-vue", JSON.stringify(this.tareas));
     },
     editar(index){
       //console.log("click", index)
       this.tareas.splice(index, 1, { nombre:this.modificacion, estado:false });
       this.modificacion="";
       
-      localStorage.setItem("gym-vue", JSON.stringify(this.tareas));
-
+      localStorage.setItem("my-vue", JSON.stringify(this.tareas));
     }
   },
 
   created: function () {
-    let datosDB = JSON.parse(localStorage.getItem("gym-vue"));
+    let datosDB = JSON.parse(localStorage.getItem("my-vue"));
     if (datosDB == null) {
-      this.tareas = [];
+      this.tareas = []
     } else {
       this.tareas = datosDB;
     }
   },
+
+  computed:{
+    color(){
+      return {
+        'bg-success':this.contador <= 10,
+        'bg-warning':this.contador > 10 && this.contador < 20,
+        'bg-danger' :this.contador >= 20
+      }
+    }
+  }
 });
